9b78d9de48f1c541d6096e545f5f103fb93501ec
William Marstrand <wdmdevelop@gmail.com>
2024-05-09 17:00:17 +0200

    add Latex LSP support

---
+++ .config/nvim/lua/plugins/lsp/latex.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+function M.setup()
+  require('lspconfig').texlab.setup{
+    settings = {
+      texlab = {
+        build = {
+          executable = "latexmk",
+          args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+          onSave = true  -- Automatically build on save
+        },
+        forwardSearch = {
+          executable = "zathura",  -- Replace with your preferred PDF viewer
+          args = { "--synctex-forward", "%l:1:%f", "%p" }
+        },
+        lint = {
+          onChange = true,  -- Enable linting as you type
+          onSave = true     -- Lint when saving the file
+        },
+        chktex = {
+          onOpenAndSave = true,  -- Run ChkTeX upon file opening or saving
+          onEdit = true          -- Run ChkTeX on each edit
+        },
+        latexindent = {
+          modifyLineBreaks = true  -- Reformat line breaks when indenting
+        }
+      }
+    }
+  }
+end
+
+return M
+
