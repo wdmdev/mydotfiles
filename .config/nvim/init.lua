cce72ff86aabc7836d16b5be1ca3bf9916ec7404
William Marstrand <wdmdevelop@gmail.com>
2024-05-15 22:03:48 +0200

    remove relative line numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 -- Basic settings
 vim.o.number = true -- Enable line numbers
-vim.o.relativenumber = true -- Enable relative line numbers
 vim.o.tabstop = 4 -- Number of spaces a tab represents
 vim.o.shiftwidth = 4 -- Number of spaces for each indentation
 vim.o.expandtab = true -- Convert tabs to spaces
