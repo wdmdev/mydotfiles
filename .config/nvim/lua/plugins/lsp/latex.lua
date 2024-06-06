b74da3dc7e127e9837d15741edaa3a08df12acc2
William Marstrand <wdmdevelop@gmail.com>
2024-06-06 15:18:48 +0200

    update latex lsp

--- .config/nvim/lua/plugins/lsp/latex.lua
+++ .config/nvim/lua/plugins/lsp/latex.lua
@@ -4,16 +4,6 @@ function M.setup()
   require('lspconfig').texlab.setup{
     settings = {
       texlab = {
-        build = {
-          executable = "latexmk",
-          args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-          onSave = true,  -- Automatically build on save
-          forwardSearchAfter = true
-        },
-        forwardSearch = {
-          executable = "zathura",  -- Replace with your preferred PDF viewer
-          args = { "--synctex-forward", "%l:1:%f", "%p" }
-        },
         lint = {
           onChange = true,  -- Enable linting as you type
           onSave = true     -- Lint when saving the file
